cd06e32804c744402705a292a17a21366d06651d
Andrei Margaev <andrei.margaev@essent.nl>
2026-06-08 13:09:50 +0200

    updates

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -139,7 +139,7 @@ return {
           spinner = {},
         },
       }
-      vim.keymap.set({ 'n', 'v' }, '<C-a>', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
+      vim.keymap.set({ 'n', 'v' }, '<leader>ct', '<cmd>CodeCompanionActions<cr>', { noremap = true, silent = true })
       vim.keymap.set({ 'n', 'v' }, '<leader>cc', '<cmd>CodeCompanionChat Toggle<cr>', { noremap = true, silent = true })
       vim.keymap.set({ 'n', 'v' }, '<leader>cn', '<cmd>CodeCompanionChat<cr>', { noremap = true, silent = true })
       vim.keymap.set('v', '<leader>ca', '<cmd>CodeCompanionChat Add<cr>', { noremap = true, silent = true })
